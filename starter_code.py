c3155c764169c93dd943579bd33d1d209e9cbf25
martiner000 <martiner000@warren.k12.in.us>
2026-04-23 11:56:27 -0400

    finished day 5

--- starter_code.py
+++ starter_code.py
@@ -36,26 +36,91 @@ class Game():
         self.player_hit_sound = pygame.mixer.Sound("player_hit.wav")
         #Set font
         self.font = pygame.font.Font("Facon.ttf", 32)
+
     def update(self):
         """Update the game"""
         self.shift_aliens()
         self.check_collisions()
         self.check_round_completion()
+
     def draw(self):
         """Draw the HUD and other information to display"""
-        pass
+        WHITE = (255, 255, 255)
+
+        # Set text
+        score_text = self.font.render(f"Score: {self.score}", True, WHITE)
+        score_rect = score_text.get_rect()
+        score_rect.centerx = WINDOW_WIDTH // 2
+        score_rect.top = 10
+
+        round_text = self.font.render(f"Round: {self.round_number}", True, WHITE)
+        round_rect = round_text.get_rect()
+        round_rect.topleft = (20, 10)
+
+        lives_text = self.font.render(f"Lives: {self.player.lives}", True, WHITE)
+        lives_rect = lives_text.get_rect()
+        lives_rect.topright = (WINDOW_WIDTH - 20, 10)
+
+        #Blit the HUD to the display
+        display_surface.blit(score_text, score_rect)
+        display_surface.blit(round_text, round_rect)
+        display_surface.blit(lives_text, lives_rect)
+        pygame.draw.line(display_surface, WHITE, (0, 50), (WINDOW_WIDTH, 50), 4)
+        pygame.draw.line(display_surface, WHITE, (0, WINDOW_HEIGHT - 100), (WINDOW_WIDTH, WINDOW_HEIGHT - 100), 4)
+
 
     def shift_aliens(self):
         """Shift a wave of aliens down the screen and reverse direction"""
-        pass
+
+        #Determine if alien_group has hit an edge
+        shift = False
+        for alien in (self.alien_group.sprites()):
+            if alien.rect.left <= 0 or alien.rect.right >= WINDOW_WIDTH:
+                shift = True
+
+        #Shift every alien down, change direction, and check for a breach
+        if shift:
+            breach = False
+            for alien in (self.alien_group.sprites()):
+                #Shift down
+                alien.rect.y  += 10 * self.round_number
+
+                #Reverse the direction and move the alien off the edge so 'shift' doesn't trigger
+                alien.direction  *= -1
+                alien.rect.x += alien.direction * alien.velocity
+
+                #Check if an alien reached the ship
+                if alien.rect.bottom >= WINDOW_HEIGHT - 100:
+                    breach = True
+
+            #Aliens breached the line
+            if breach:
+                self.breach_sound.play()
+                self.player.lives -= 1
+                self.check_game_status("Aliens breach the line", "Press 'Enter' to continue")
 
     def check_collisions(self):
         """Check for collisions"""
-        pass
+        #See if any bullet in the player bullet group hits an alien in the alien group
+        if pygame.sprite.groupcollide(self.player_bullet_group, self.alien_group, True, True):
+            self.alien_hit_sound.play()
+            self.score += 100
+
+        #See if the player has collided with any bullet in the alien bullet group
+        if pygame.sprite.spritecollide(self.player, self.alien_bullet_group, True):
+            self.player_hit_sound.play()
+            self.player.lives -= 1
+
+            self.check_game_status("You've been hit!", "Press 'Enter' to continue")
+
+
 
     def check_round_completion(self):
         """Check to see if a player has completed a single round"""
-        pass
+        if not (self.alien_group):
+            self.score += 1000 * self.round_number
+            self.round_number += 1
+            self.start_new_round()
 
     def start_new_round(self):
         """Start a new round"""
@@ -63,20 +128,24 @@ class Game():
 
         for col in range(11):
             for row in range(5):
-                Alien(64 + col * 64, 64 + row * 64, self.round_number, self.alien_bullet_group)
-                # first arg (x) is going to be 64 + col * 64
-                # second arg (y) is going to be 64 + row * 64
-                # third arg (velocity) is going to be self.round_number
-                # fourth arg is self.alien_bullet_group
-                self.alien_bullet_group.add(Alien)
-        # Pause the game and prompt user to start
-        # first arg (main_text) f"Space Invaders Round {self.round_number}"
-        # second arg (sub_text) "Press 'Enter' to being"
+                alien = Alien(64 + col * 64, 64 + row * 64, self.round_number, self.alien_bullet_group)
+                self.alien_group.add(alien)
+
         self.new_round_sound.play()
         self.pause_game(f"Space Invaders Round {self.round_number}", "Press 'Enter' to begin")
+
     def check_game_status(self, main_text, sub_text):
         """Check to see the status of the game and how the player died"""
-        pass
+        self.alien_bullet_group.empty()
+        self.player_bullet_group.empty()
+        self.player.reset()
+        for alien in self.alien_group:
+            alien.reset()
+
+        if self.player.lives == 0:
+            self.pause_game(main_text, sub_text)
+        else:
+            self.reset_game()
 
     def pause_game(self, main_text, sub_text):
         """Pauses the game"""
@@ -88,22 +157,23 @@ class Game():
         # Create main pause text
         main_text = self.font.render(main_text, True, WHITE)
         main_rect = main_text.get_rect()
+        main_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
         #Create sub pause text
         sub_text = self.font.render(sub_text, True, WHITE)
         sub_rect = sub_text.get_rect()
         sub_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 64)
+
         #Blit the pause text
         display_surface.fill(BLACK)
         display_surface.blit(main_text, main_rect)
         display_surface.blit(sub_text, sub_rect)
         pygame.display.update()
+
         #Pause the game until the user hits enter
-        #TODO: assign True to is_paused
-        #TODO: while is_paused:
         is_paused = True
         while is_paused:
             for event in pygame.event.get():
-            #TODO: for event in pygame.event.get():
                 # The user wants to play again
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
@@ -123,10 +193,12 @@ class Game():
         self.score = 0
         self.round_number = 1
         self.player.lives = 5
+
         # Empty groups
         self.alien_group.empty()
         self.alien_bullet_group.empty()
         self.player_bullet_group.empty()
+
         # Start a new game
         self.start_new_round()
 
@@ -240,7 +312,7 @@ class AlienBullet(pygame.sprite.Sprite):
         super().__init__()
 
         self.image = pygame.image.load("red_laser.png")
-        self.image = self.image.set.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
 
@@ -277,14 +349,13 @@ my_game.start_new_round()
 running = True
 while running:
     for event in pygame.event.get():
-
         # Check to see if the user wants to quit
         if event.type == pygame.QUIT:
             running = False
 
         #The player wants to fire
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+            if event.key == pygame.K_SPACE:
                 my_player.fire()
 
     #Fill the display
@@ -306,6 +377,10 @@ while running:
 
 
     #Update and draw a Game object
+    my_game.update()
+    my_game.draw()
+
+
     pygame.display.update()
 
     #Update the display and tick the clock
